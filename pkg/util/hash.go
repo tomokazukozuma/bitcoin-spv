038afd90bd5ba9b655deedffa6196da9886ba84e
Tomokazu Kozuma <tomokazu.kozuma@gmail.com>
2019-12-29 15:18:25 +0900

    HASH160を実装

--- pkg/util/hash.go
+++ pkg/util/hash.go
@@ -1,9 +1,21 @@
 package util
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"io"
+
+	"golang.org/x/crypto/ripemd160"
+)
 
 func Hash256(b []byte) []byte {
 	hash1 := sha256.Sum256(b)
 	hash2 := sha256.Sum256(hash1[:])
 	return hash2[:]
+}
+
+func Hash160(b []byte) []byte {
+	sum := sha256.Sum256(b)
+	rip := ripemd160.New()
+	io.WriteString(rip, string(sum[:]))
+	return rip.Sum(nil)
 }
