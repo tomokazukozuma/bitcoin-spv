88750593a59e95eefd8a287d6bda57f019582bde
Tomokazu Kozuma <tomokazu.kozuma@gmail.com>
2020-01-04 11:43:37 +0900

    DecodeGetData()を追加

--- pkg/protocol/message/getdata.go
+++ pkg/protocol/message/getdata.go
@@ -36,3 +36,17 @@ func (g *GetData) Encode() []byte {
 		bytes.Join(inventoryBytes, []byte{}),
 	}, []byte{})
 }
+
+func DecodeGetData(b []byte) (*GetData, error) {
+	count, _ := common.DecodeVarInt(b)
+	b = b[len(count.Encode()):]
+	var inventory []*InvVect
+	for i := 0; uint64(i) < count.Data; i++ {
+		iv, _ := DecodeInvVect(b[:36*(i+1)])
+		inventory = append(inventory, iv)
+	}
+	return &GetData{
+		Count:     count,
+		Inventory: inventory,
+	}, nil
+}
