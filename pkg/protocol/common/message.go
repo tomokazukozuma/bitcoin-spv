5b48d10c2e4811cd1eabc533b0628d8ea1fe4792
Tomokazu Kozuma <tomokazu.kozuma@gmail.com>
2020-01-04 11:42:44 +0900

    IsTestnet3()を追加

--- pkg/protocol/common/message.go
+++ pkg/protocol/common/message.go
@@ -8,7 +8,10 @@ import (
 	"github.com/tomokazukozuma/bitcoin-spv/pkg/util"
 )
 
-const MessageLen = 24
+const (
+	MagicTestnet3 = uint32(118034699)
+	MessageLen    = 24
+)
 
 type Message struct {
 	Magic    uint32
@@ -23,7 +26,7 @@ func NewMessage(command [12]byte, payload []byte) *Message {
 	hashedMsg := util.Hash256(payload)
 	copy(checksum[:], hashedMsg[0:4])
 	return &Message{
-		Magic:    binary.LittleEndian.Uint32([]byte{0x0B, 0x11, 0x09, 0x07}),
+		Magic:    MagicTestnet3,
 		Command:  command,
 		Length:   uint32(len(payload)),
 		Checksum: checksum,
@@ -71,7 +74,9 @@ func IsValidChecksum(checksum [4]byte, payload []byte) bool {
 	hashedPayload := util.Hash256(payload)
 	var payloadChecksum [4]byte
 	copy(payloadChecksum[:], hashedPayload[0:4])
-	log.Printf("checksum: %+v", checksum)
-	log.Printf("payloadChecksum: %+v", payloadChecksum)
 	return checksum == payloadChecksum
+}
+
+func IsTestnet3(magic uint32) bool {
+	return magic == MagicTestnet3
 }
